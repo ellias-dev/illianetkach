6fe8d504dd0a0b3c93cbbad1f5c091438727078b
ellias-dev <elliaswd@pm.me>
2022-11-16 21:10:47 +0400

    links in connect and footer

--- src/react-components/footer.js
+++ src/react-components/footer.js
@@ -5,12 +5,12 @@ export const Footer = () => {
     return (
         <footer className="container-fluid">
             <div className='footer-connect d-flex justify-content-around flex-wrap'>
-                <a href='#'><i className="fa-brands fa-bandcamp"></i></a>
-                <a href='#'><i className="fa-brands fa-spotify"></i></a>
-                <a href='#'><i className="fa-brands fa-itunes"></i></a>
-                <a href='#'><i className="fa-brands fa-twitter"></i></a>
-                <a href='#'><i className="fa-brands fa-facebook-f"></i></a>
-                <a href='#'><i className="fa-brands fa-instagram"></i></a>
+                <a href='https://illianetkach.bandcamp.com/' target='_blank'><i className="fa-brands fa-bandcamp"></i></a>
+                <a href='https://open.spotify.com/artist/1Qs1Bm7Y1KQBmPV1ailUG2' target='_blank'><i className="fa-brands fa-spotify"></i></a>
+                <a href='https://music.apple.com/us/artist/ilya-netkach/641932434' target='_blank'><i className="fa-brands fa-itunes"></i></a>
+                <a href='https://twitter.com/illianetkach' target='_blank'><i className="fa-brands fa-twitter"></i></a>
+                <a href='https://www.facebook.com/IlyaNetkach/' target='_blank'><i className="fa-brands fa-facebook-f"></i></a>
+                <a href='https://instagram.com/illianetkah' target='_blank'><i className="fa-brands fa-instagram"></i></a>
             </div>
             
             &copy; 2022 Illia Netkach. All Rights Reserved
--- src/react-components/home/connect.js
+++ src/react-components/home/connect.js
@@ -6,12 +6,12 @@ export const Connect = () => {
     return (
         <div className='connect'>
             <div className='fa-connect d-flex justify-content-around flex-wrap'>
-                <a href='#'><i className="fa-brands fa-bandcamp"></i> BandCamp</a>
-                <a href='#'><i className="fa-brands fa-spotify"></i> Spotify</a>
-                <a href='#'><i className="fa-brands fa-itunes"></i> Apple Music</a>
-                <a href='#'><i className="fa-brands fa-twitter"></i> Twitter</a>
-                <a href='#'><i className="fa-brands fa-facebook-f"></i> Facebook</a>
-                <a href='#'><i className="fa-brands fa-instagram"></i> Instagram</a>
+                <a href='https://illianetkach.bandcamp.com/' target='_blank'><i className="fa-brands fa-bandcamp"></i> BandCamp</a>
+                <a href='https://open.spotify.com/artist/1Qs1Bm7Y1KQBmPV1ailUG2' target='_blank'><i className="fa-brands fa-spotify"></i> Spotify</a>
+                <a href='https://music.apple.com/us/artist/ilya-netkach/641932434' target='_blank'><i className="fa-brands fa-itunes"></i> Apple Music</a>
+                <a href='https://twitter.com/illianetkach' target='_blank'><i className="fa-brands fa-twitter"></i> Twitter</a>
+                <a href='https://www.facebook.com/IlyaNetkach/' target='_blank'><i className="fa-brands fa-facebook-f"></i> Facebook</a>
+                <a href='https://instagram.com/illianetkach' target='_blank'><i className="fa-brands fa-instagram"></i> Instagram</a>
             </div>
             {/* <div className='home-form'>
                 <HomeForm />
