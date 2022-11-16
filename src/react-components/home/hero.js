ec5f3c0b01e480bf5a08344392be455ab18ef84c
ellias-dev <elliaswd@pm.me>
2022-11-17 00:20:00 +0400

    back to jpg unfortunately

--- src/react-components/home/hero.js
+++ src/react-components/home/hero.js
@@ -3,13 +3,13 @@ import React from 'react';
 export const Hero = () => {
     return (
         <div className="jumbotron jumbotron-fluid jumbo">
-            {/* <img width='100%' src='/images/sliderhigh.webp'></img>  */}
+            <img width='100%' src='/images/sliderhigh.jpg'></img> 
 
 
-            <picture>
-    <source srcset="/images/sliderhigh.webp" width='100%'/>
-    <img src="/images/sliderhigh.jpg" alt="" width='100%' />
-</picture>
+            
+    
+    
+
         </div>
     )
     
