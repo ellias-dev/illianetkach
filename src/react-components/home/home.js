0efc0178ccc23cfca08dd4e92d74692e2e878d1e
ellias-dev <elliaswd@pm.me>
2022-11-16 22:07:14 +0400

    no out now test speed

--- src/react-components/home/home.js
+++ src/react-components/home/home.js
@@ -11,7 +11,7 @@ export const Home = () => {
         <div>
             <Hero />
             <Connect />
-            <Listen />
+            {/* <Listen /> */}
             <LatestNews />
             {/* <RecentVideo /> */}
             <Press />
