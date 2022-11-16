6011096063c62bfdfa4cea241cbe02dd74069423
ellias-dev <elliaswd@pm.me>
2022-11-16 21:30:02 +0400

    press links

--- src/react-components/home/press.js
+++ src/react-components/home/press.js
@@ -19,14 +19,14 @@ export const Press = () => {
             <div className="row">
             <div className="press col-12 col-md-6">
                 <p>"Ilya Netkach stands without any ifs and buts to his instrument and his music. I AM is that what makes him. Either one stands for his music or not. An intermediate it does not allow..."<br />
-                © Hans-Bernd Hulsmann, "Smooth & Soul"</p>
+                <a href='#' target='_blank'>© Hans-Bernd Hulsmann, "Smooth & Soul"</a></p>
             
 
             </div>
 <div className="press col-12 col-md-6">
                <p>
              "Netkach stays frank and spontaneous in the moment while choosing each note with care, and the result is an exotic work that feels over in a blink, but still says a lot in a short time."<br />
-© Geno Thackara, "All About Jazz"
+             <a href='#' target='_blank'>© Geno Thackara, "All About Jazz"</a>
                </p>
            </div>
             </div>
