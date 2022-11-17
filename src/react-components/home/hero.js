6f1e7b39a8bd068d3cf7500842aca110b8b5d63b
ellias-dev <elliaswd@pm.me>
2022-11-17 22:36:17 +0400

    jumbotron optimized

--- src/react-components/home/hero.js
+++ src/react-components/home/hero.js
@@ -3,7 +3,13 @@ import React from 'react';
 export const Hero = () => {
     return (
         <div className="jumbotron jumbotron-fluid jumbo">
-            <img width='100%' src='/images/sliderhigh.jpg'></img> 
+            {/* <img width='100%' src='/images/sliderhigh.jpg'></img>  */}
+
+            <picture>
+                <source media="(min-width: 768px)" srcset="./images/sliderhigh1200.jpg" width='100%'></source>
+                <source media="(min-width: 1200px)" srcset="./images/sliderhigh.jpg" width='100%'></source>
+                <img src="./images/sliderhigh768.jpg" width='100%'></img>
+            </picture>
 
 
             
